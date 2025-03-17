85b876c71925cd5658eac7d192a55ada75feb242
hp <bhagatnicky1234@gmail.com>
2025-03-17 19:39:00 +0530

    personal&team Task

--- lib/AddPersonalTask.dart
+++ lib/AddPersonalTask.dart
@@ -5,14 +5,12 @@ class AddPersonalTask extends StatefulWidget {
   @override
   _AddTaskState createState() => _AddTaskState();
 }
-
 class _AddTaskState extends State<AddPersonalTask> {
   TextEditingController _dateController = TextEditingController();
   TextEditingController _timeController = TextEditingController();
-
+  var task = TextEditingController();
   // Dropdown selected value
   String selectedPriority = 'Personal';
-
   /// Function to show Date Picker
   void _pickDate() async {
     DateTime? pickedDate = await showDatePicker(
@@ -21,7 +19,6 @@ class _AddTaskState extends State<AddPersonalTask> {
       firstDate: DateTime(2000),
       lastDate: DateTime(2100),
     );
-
     if (pickedDate != null) {
       setState(() {
         _dateController.text =
@@ -36,7 +33,6 @@ class _AddTaskState extends State<AddPersonalTask> {
       context: context,
       initialTime: TimeOfDay.now(),
     );
-
     if (pickedTime != null) {
       setState(() {
         _timeController.text = pickedTime.format(context);
@@ -58,88 +54,91 @@ class _AddTaskState extends State<AddPersonalTask> {
       ),
       body: Container(
         margin: EdgeInsets.only(top: 40),
-        child: Column(
-          children: [
-            Text('What is to be done? \t \t \t', style: TextStyle(fontSize: 20, color: appbarcolor)),
-            Container(
-              width: 250,
-              height: 35,
-              margin: EdgeInsets.only(left: 40),
-              child: TextField(
-                  style: TextStyle(fontSize: 18)
+        child:SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('What is to be done? \t \t \t', style: TextStyle(fontSize: 20, color: appbarcolor)),
+              Container(
+                width: 250,
+                height: 35,
+                margin: EdgeInsets.only(left: 40),
+                child: TextField(
+                  controller: task,
+                    style: TextStyle(fontSize: 18)
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Text('Due date \t \t \t \t \t \t \t \t \t \t \t \t', style: TextStyle(fontSize: 20, color: appbarcolor)),
-            Container(
-              width: 250,
-              height: 30,
-              margin: EdgeInsets.only(left: 40),
-              child: TextField(
-                controller: _dateController,
-                readOnly: true,
-                style: TextStyle(fontSize: 18),
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    onPressed: _pickDate,
-                    icon: Icon(Icons.date_range,color: appbarcolor,),
+              SizedBox(height: 20),
+              Text('Due date \t \t \t \t \t \t \t \t \t \t \t \t', style: TextStyle(fontSize: 20, color: appbarcolor)),
+              Container(
+                width: 250,
+                height: 30,
+                margin: EdgeInsets.only(left: 40),
+                child: TextField(
+                  controller: _dateController,
+                  readOnly: true,
+                  style: TextStyle(fontSize: 18),
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: _pickDate,
+                      icon: Icon(Icons.date_range,color: appbarcolor,),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Container(
-              width: 250,
-              height: 30,
-              margin: EdgeInsets.only(left: 40),
-              child: TextField(
-                controller: _timeController,
-                readOnly: true,
-                style: TextStyle(fontSize: 18),
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    onPressed: _pickTime,
-                    icon: Icon(Icons.timer_outlined,color: appbarcolor,),
+              SizedBox(height: 10),
+              Container(
+                width: 250,
+                height: 30,
+                margin: EdgeInsets.only(left: 40),
+                child: TextField(
+                  controller: _timeController,
+                  readOnly: true,
+                  style: TextStyle(fontSize: 18),
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      onPressed: _pickTime,
+                      icon: Icon(Icons.timer_outlined,color: appbarcolor,),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Text('Add to \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t', style: TextStyle(fontSize: 20, color: appbarcolor)),
-            Container(
-              width: 250,
-              margin: EdgeInsets.only(left: 40),
-              child: DropdownButton<String>(
-                value: selectedPriority,
-                isExpanded: true,
-                items: ['Personal', 'Team'].map((String priority) {
-                  return DropdownMenuItem<String>(
-                    value: priority,
-                    child: Text(priority, style: TextStyle(fontSize: 18)),
-                  );
-                }).toList(),
-                onChanged: (newValue) {
-                  setState(() {
-                    selectedPriority = newValue!;
-                  });
-                },
-              ),
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  minimumSize: Size(180, 40), // Increased button size
+              SizedBox(height: 20),
+              Text('Add to \t \t \t \t \t \t \t \t \t \t \t \t \t \t \t', style: TextStyle(fontSize: 20, color: appbarcolor)),
+              Container(
+                width: 250,
+                margin: EdgeInsets.only(left: 40),
+                child: DropdownButton<String>(
+                  value: selectedPriority,
+                  isExpanded: true,
+                  items: ['Personal', 'Team'].map((String priority) {
+                    return DropdownMenuItem<String>(
+                      value: priority,
+                      child: Text(priority, style: TextStyle(fontSize: 18)),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    setState(() {
+                      selectedPriority = newValue!;
+                    });
+                  },
                 ),
-                child: Text('Add', style: TextStyle(fontSize: 22, color: Colors.white)), // Increased font size
               ),
-            ),
-          ],
+              SizedBox(height: 30),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    minimumSize: Size(180, 40), // Increased button size
+                  ),
+                  child: Text('Add', style: TextStyle(fontSize: 22, color: Colors.white)), // Increased font size
+                ),
+              ),
+            ],
+          ),
         ),
       )
     );
--- lib/AddTeamTask.dart
+++ lib/AddTeamTask.dart
@@ -4,103 +4,123 @@ import 'package:taskmanager/Colors.dart';
 import 'package:taskmanager/DashboardScreen.dart';
 import 'package:taskmanager/TeamScreen.dart';
 
-class AddTeamTask extends StatelessWidget{
-
-  var assignTeamTask = TextEditingController();
+class AddTeamTask extends StatefulWidget {
+  @override
+  _AddTaskState createState() => _AddTaskState();
+}
+class _AddTaskState extends State<AddTeamTask> {
+  TextEditingController _dateController = TextEditingController();
+  TextEditingController _timeController = TextEditingController();
+  var assignTask = TextEditingController();
   var assignTo = TextEditingController();
   var comment = TextEditingController();
 
+  /// Function to show Date Picker
+  void _pickDate() async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (pickedDate != null) {
+      setState(() {
+        _dateController.text =
+        "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 54,  // Optional, appbar height
-        leadingWidth: 37,
-        title: Text('Powering Personal and Team Task', style: TextStyle(fontSize: 18, color: Colors.white),),
-        backgroundColor: appbarcolor,
-      ),
-
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              controller: assignTeamTask,
-              style: TextStyle(fontSize: 20, color: Colors.black),    // increase font size
-              decoration: InputDecoration(
-                labelText: 'Assign Task',
-                labelStyle: TextStyle(color: Colors.black87, fontSize: 20),
-                /*focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: borderColor, width:2)
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Colors.black, width: 2)
-                )*/   // For Border
-              ),
-            ),
+        appBar: AppBar(
+          toolbarHeight: 54,
+          leadingWidth: 37,
+          title: Text(
+            'Powering Personal and Team Task',
+            style: TextStyle(fontSize: 18, color: Colors.white),
           ),
-
-          SizedBox(height: 8,),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: TextField(
-              controller: assignTo,
-              style: TextStyle(fontSize: 20, color: Colors.black),    // increase font size
-              decoration: InputDecoration(
-                  labelText: 'Assign To',
-                  labelStyle: TextStyle(color: Colors.black87, fontSize: 20),
-               /*   focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: borderColor, width:2)
+          backgroundColor: Colors.blue,
+        ),
+        body: Container(
+          margin: EdgeInsets.only(top: 40),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Asign Task \t \t \t \t \t \t \t \t \t \t ', style: TextStyle(fontSize: 20, color: appbarcolor)),
+                Container(
+                  width: 250,
+                  height: 35,
+                  margin: EdgeInsets.only(left: 40),
+                  child: TextField(
+                    controller: assignTask,
+                      style: TextStyle(fontSize: 18)
                   ),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.black, width: 2)
-                  )*/       // For Border
-              ),
+                ),
+                SizedBox(height: 20),
+                Text('Asign To \t \t \t \t \t \t \t \t \t \t \t \t', style: TextStyle(fontSize: 20, color: appbarcolor)),
+                Container(
+                  width: 250,
+                  height: 35,
+                  margin: EdgeInsets.only(left: 40),
+                  child: TextField(
+                    controller: assignTo,
+                      style: TextStyle(fontSize: 18)
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text('Due date \t \t \t \t \t \t \t \t \t \t \t \t', style: TextStyle(fontSize: 20, color: appbarcolor)),
+                Container(
+                  width: 250,
+                  height: 30,
+                  margin: EdgeInsets.only(left: 40),
+                  child: TextField(
+                    controller: _dateController,
+                    readOnly: true,
+                    style: TextStyle(fontSize: 18),
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: _pickDate,
+                        icon: Icon(Icons.date_range,color: appbarcolor,),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text('Comment \t \t \t \t \t \t \t \t \t \t \t \t', style: TextStyle(fontSize: 20, color: appbarcolor)),
+                Container(
+                  height: 180,
+                  width: 280,
+                  margin: EdgeInsets.only(left: 50,top: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black)
+                  ),
+                  child: TextField(
+                    controller: comment,
+                    style: TextStyle(fontSize: 20, color: Colors.black),    // increase font size
+                    maxLines: null,   // Allow multiple lines
+                    expands: true,    // Expands to fill the container
+
+                  ),
+                ),
+                SizedBox(height: 30),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      minimumSize: Size(180, 40), // Increased button size
+                    ),
+                    child: Text('Assign', style: TextStyle(fontSize: 22, color: Colors.white)), // Increased font size
+                  ),
+                ),
+              ],
             ),
           ),
-
-          SizedBox(height: 9,),
-          Container(
-            height: 180,
-            width: 320,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black)
-            ),
-            child: TextField(
-              controller: comment,
-              style: TextStyle(fontSize: 20, color: Colors.black),    // increase font size
-              maxLines: null,   // Allow multiple lines
-              expands: true,    // Expands to fill the container
-              decoration: InputDecoration(
-                labelText: 'Comment',
-                labelStyle: TextStyle(color: Colors.black, fontSize: 18),
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.all(8)
-              ),
-            ),
-          ),
-
-          SizedBox(height: 12,),
-          ElevatedButton(onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => Dashboard()));
-          },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: appbarcolor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)
-                )
-              ),
-              child: Text('Assign', style: TextStyle(fontSize: 20, color: Colors.white),))
-
-        ],
-      ),
-
+        )
     );
   }
-
 }
